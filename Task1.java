72a413cba4238fdb2fde824dd6835e97f8132237
suborg4 <114426899+suborg4@users.noreply.github.com>
2023-07-06 20:06:47 +0500

    final commit

---
+++ Task1.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Напишите программу, которая запрашивает у пользователя два целых числа и выполняет их деление.
+ * Если второе число равно нулю, выбросите исключение ArithmeticException с сообщением "Деление на ноль недопустимо".
+ * Иначе выведите результат деления на экран.
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Введите первое число: ");
+            int num1 = sc.nextInt();
+            System.out.print("Введите первое число: ");
+            int num2 = sc.nextInt();
+            System.out.printf("%d / %d = %d", num1, num2, division(num1, num2));
+        } catch (InputMismatchException e){
+            System.out.println("Не корректный ввод!");
+        }
+    }
+
+    public static int division(int num1, int num2){
+        if (num2 == 0){
+            throw new ArithmeticException("Деление на ноль недопустимо");
+        }
+        return num1 / num2;
+    }
+}
